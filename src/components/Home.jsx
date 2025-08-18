91bfe628ee8b948803473a1bc504a5ae1f3a12bb
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2025-08-18 11:04:51 -0600

    feat: create nav and home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home(){
+
+
+    return(<div>
+        <button>Shop Now</button>
+    </div>)
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+
+export default function Nav(){
+
+
+
+    return(<ul>
+        <li>Home</li>
+        <li>Shop</li>
+    </ul>)
+}
